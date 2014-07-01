84afa2e6f999f8b6385b50dce6f03e917ea924cc
bomdia <bellatranello@gmail.com>
2014-07-01 23:38:36 +0200

    deleted the banner v1 from v2 branch

--- banner/lib/get.php
+++
@@ -1,9 +0,0 @@
-<?php
-//sostituisce le impostazioni con le variabili get
-if(isset($_GET['host'])){$host=$_GET['host'];}
-if(isset($_GET['port'])){$port=$_GET['port'];}
-if(isset($_GET['stile'])){$stile=$_GET['stile'];}
-if(isset($_GET['url'])){$url=$_GET['url'];}
-if(isset($_GET['colore'])){$colore=$_GET['colore'];}
-if(isset($_GET['r'],$_GET['g'],$_GET['b'])){$r=$_GET['r'];$g=$_GET['g'];$b=$_GET['b'];if($r=='o'){$r=0;}if($g=='o'){$g=0;}if($b=='o'){$b=0;}}
-?>
